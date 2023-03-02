47797cbda3d2d6c07e9a99364d9535239dc1ebc0
reymartvega@gmail.com <reymartvega@gmail.com>
2023-03-03 00:08:12 +0800

    Add getCostumers route

--- controllers/client.js
+++ controllers/client.js
@@ -1,5 +1,7 @@
+import { response } from "express";
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
+import User from "../models/User.js";
 
 export const getProducts = async(request,response) =>{
     try {
@@ -19,4 +21,14 @@ export const getProducts = async(request,response) =>{
     } catch (error) {
         response.status(404).json({message:error.message});
     }  
+}
+export const getCustomers= async(request,response) =>{
+    try {
+        const customers = await User.find({role: "user"}).select('-password')
+        return (
+            response.status(200).json(customers)
+        )
+    } catch (error) {
+        response.status(404).json({message:error.message});
+    }
 }
